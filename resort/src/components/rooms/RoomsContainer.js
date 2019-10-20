e6b913974449343afe71ef172f24e07326fb26c7
imon3 <imonovbude@gmail.com>
2019-10-20 01:16:41 -0700

    created higher order function for context value in context.js

--- resort/src/components/rooms/RoomsContainer.js
+++ resort/src/components/rooms/RoomsContainer.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import RoomsFilter from './RoomsFilter';
 import RoomsList from './RoomsList';
+import { withRoomConsumer } from '../../Context';
+import Loading from '../loading/Loading';
 
-const RoomsContainer = () => {
-  return (
-    <div>
-      Hello From Rooms Container
-      <RoomsFilter />
-      <RoomsList />
-    </div>
-  );
+const RoomsContainer = props => {
+  return <div></div>;
 };
 
-export default RoomsContainer;
+export default withRoomConsumer(RoomsContainer);
+
+// import React from 'react';
+// import RoomsFilter from './RoomsFilter';
+// import RoomsList from './RoomsList';
+// import { RoomConsumer } from '../../Context';
+// import Loading from '../loading/Loading';
+
+// const RoomsContainer = () => {
+//   return (
+//     <RoomConsumer>
+//       {value => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+//         return (
+//           <div>
+//             Hello From Rooms Container
+//             <RoomsFilter rooms={rooms} />
+//             <RoomsList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// };
+
+// export default RoomsContainer;
