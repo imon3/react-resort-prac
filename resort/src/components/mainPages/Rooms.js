a0a212a7d9a42c114ad44c68c89f7446429ec411
imon3 <imonovbude@gmail.com>
2019-10-19 17:52:35 -0700

    setup rooms page header section

--- resort/src/components/mainPages/Rooms.js
+++ resort/src/components/mainPages/Rooms.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import Hero from '../hero/Hero';
+import Banner from '../banner/Banner';
+import { Link } from 'react-router-dom';
 
 const Rooms = () => {
-  return <Hero hero='roomsHero' />;
+  return (
+    <Hero hero='roomsHero'>
+      <Banner title='our rooms'>
+        <Link to='/' className='btn-primary'>
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Rooms;
